600165b3599dc42bd69ef808f8598b911165abaf
tbang-350 <59138492+tbang-350@users.noreply.github.com>
2021-07-31 18:59:18 +0300

    added succesfully to database

---
+++ app/src/main/java/com/example/sqlitetest2/AddActivity.java
@@ -0,0 +1,38 @@
+package com.example.sqlitetest2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddActivity extends AppCompatActivity {
+
+    EditText title_input, author_input, pages_input;
+    Button add_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        title_input = (EditText) findViewById(R.id.title_input);
+        author_input = (EditText) findViewById(R.id.author_input);
+        pages_input = (EditText) findViewById(R.id.pages_input);
+        add_button = (Button) findViewById(R.id.add_button);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatabaseHelper myDB = new DatabaseHelper(AddActivity.this);
+                myDB.addBook(title_input.getText().toString().trim(),
+                        author_input.getText().toString().trim(),
+                        Integer.parseInt(pages_input.getText().toString().trim()));
+
+            }
+        });
+
+    }
+}
